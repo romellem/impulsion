6a42eb291d2b9ea3a544ed189dfcca0eb9d3dbf5
Matt Wade <matt.wade@designory.com>
2020-03-28 14:04:50 -0500

    Converts babel config to function export

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-	presets: [
-		'@babel/preset-env',
-	],
-	comments: false,
+module.exports = function babelConfig(api) {
+	api.cache(true);
+
+	return {
+		presets: [
+			'@babel/preset-env',
+		],
+		comments: false,
+	};
 };
