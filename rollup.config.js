2ab3850c68ec004a09ea5e7cf5c33349b6e1d983
romellem <romellem.git@gmail.com>
2020-09-29 16:51:06 -0500

    Uses non-deprecated rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 
@@ -16,7 +16,7 @@ const makeRollupConfig = (globalObjectName, inputFile, outputFile) => {
 		plugins: [
 			resolve(),
 			commonjs(),
-			babel(),
+			babel({ babelHelpers: 'bundled' }),
 		],
 	};
 
